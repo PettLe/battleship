244c27540bfb09da7f5bcc92ad521c8afdd42b13
PettLe <petewaterz@gmail.com>
2021-11-12 10:06:57 +0200

    had to make changes to hit() to make it work with receiveAttack()

--- src/gamelogic.js
+++ src/gamelogic.js
@@ -10,8 +10,11 @@ const ship = (length) => {
   }
 
   // const hit = (x, y) => (ship.occupied.find[x + y] = "x");
-  const hit = (n) => (hitBoxes[n] = "x");
-
+  // const hit = (n) => (hitBoxes[n] = "x");
+  function hit(x, y) {
+    const hitPoint = occupied.indexOf(x + y);
+    hitBoxes[hitPoint] = "x";
+  }
   function isSunk() {
     let count = 0;
     for (let i = 0; i < length; i++) {
@@ -42,6 +45,7 @@ module.exports = ship;
 const gameboard = () => {
   const shots = [];
   const boardOccupied = [];
+  const ships = [];
   function placeShip(x, y) {
     const newShip = ship(4);
     newShip.position = x + y;
@@ -49,18 +53,20 @@ const gameboard = () => {
       newShip.occupied.push(x + (y + i));
       boardOccupied.push(x + (y + i));
     }
-    return newShip.occupied;
+    ships.push(newShip);
+    // return newShip.occupied;
   }
   const receiveAttack = (x, y) => {
     shots.push(x + y);
     if (!boardOccupied.includes(x + y)) {
       return "missed!";
     }
-    // newShip.hit(x, y);
+    ships[0].hit(x, y);
     return "it's a hit!";
   };
   return {
     shots,
+    ships,
     boardOccupied,
     placeShip,
     receiveAttack,
@@ -78,4 +84,6 @@ module.exports = gameboard;
 //  console.log(newShip.destroyed);
 const newGame = gameboard();
 newGame.placeShip("B", 4);
-console.log(newGame.boardOccupied);
+console.log(newGame.ships);
+newGame.receiveAttack("B", 5);
+console.log(newGame.ships);
